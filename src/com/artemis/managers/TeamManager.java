c9ed609e743880d237936061ac9d785b5f60751c
Atamert Ölçgen <muhuk@muhuk.com>
2013-08-18 12:12:28 +0800

    Removed unused import of Entity.

--- src/com/artemis/managers/TeamManager.java
+++ src/com/artemis/managers/TeamManager.java
@@ -3,7 +3,6 @@ package com.artemis.managers;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.artemis.Entity;
 import com.artemis.Manager;
 import com.artemis.utils.Bag;
 import com.artemis.utils.ImmutableBag;
@@ -11,49 +10,36 @@ import com.artemis.utils.ImmutableBag;
 
 /**
  * Use this class together with PlayerManager.
- * 
+ *
  * You may sometimes want to create teams in your game, so that
  * some players are team mates.
- * 
+ *
  * A player can only belong to a single team.
- * 
+ *
  * @author Arni Arent
  *
  */
 public class TeamManager extends Manager {
-	private Map<String, Bag<String>> playersByTeam;
-	private Map<String, String> teamByPlayer;
+	private final Map<String, Bag<String>> playersByTeam;
+	private final Map<String, String> teamByPlayer;
 
 	public TeamManager() {
 		playersByTeam = new HashMap<String, Bag<String>>();
 		teamByPlayer = new HashMap<String, String>();
 	}
-	
-	@Override
-	protected void initialize() {
-	}
-	
-	public String getTeam(String player) {
-		return teamByPlayer.get(player);
-	}
-	
-	public void setTeam(String player, String team) {
-		removeFromTeam(player);
-		
-		teamByPlayer.put(player, team);
-		
-		Bag<String> players = playersByTeam.get(team);
-		if(players == null) {
-			players = new Bag<String>();
-			playersByTeam.put(team, players);
-		}
-		players.add(player);
-	}
-	
+
 	public ImmutableBag<String> getPlayers(String team) {
 		return playersByTeam.get(team);
 	}
-	
+
+	public String getTeam(String player) {
+		return teamByPlayer.get(player);
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
 	public void removeFromTeam(String player) {
 		String team = teamByPlayer.remove(player);
 		if(team != null) {
@@ -64,4 +50,16 @@ public class TeamManager extends Manager {
 		}
 	}
 
+	public void setTeam(String player, String team) {
+		removeFromTeam(player);
+
+		teamByPlayer.put(player, team);
+
+		Bag<String> players = playersByTeam.get(team);
+		if(players == null) {
+			players = new Bag<String>();
+			playersByTeam.put(team, players);
+		}
+		players.add(player);
+	}
 }
