d68cc34662167673fffbef931113df9d05cdc16e
Atamert Ölçgen <muhuk@muhuk.com>
2013-08-24 14:00:11 +0800

    Reformatted World.

--- src/com/artemis/World.java
+++ src/com/artemis/World.java
@@ -14,62 +14,107 @@ import com.artemis.utils.ImmutableBag;
  * 
  * You must use this to create, delete and retrieve entities.
  * 
- * It is also important to set the delta each game loop iteration, and initialize before game loop.
+ * It is also important to set the delta each game loop iteration, and
+ * initialize before game loop.
  * 
  * @author Arni Arent
  * 
  */
 public class World {
-	private EntityManager em;
-	private ComponentManager cm;
+	private static class ComponentMapperInitHelper {
 
-	public float delta;
-	private Bag<Entity> added;
-	private Bag<Entity> changed;
-	private Bag<Entity> deleted;
-	private Bag<Entity> enable;
-	private Bag<Entity> disable;
+		public static void config(Object target, World world) {
+			try {
+				Class<?> clazz = target.getClass();
+				for (Field field : clazz.getDeclaredFields()) {
+					Mapper annotation = field.getAnnotation(Mapper.class);
+					if (annotation != null
+							&& Mapper.class.isAssignableFrom(Mapper.class)) {
+						ParameterizedType genericType = (ParameterizedType) field
+								.getGenericType();
+						Class componentType = (Class) genericType
+								.getActualTypeArguments()[0];
 
-	private Map<Class<? extends Manager>, Manager> managers;
-	private Bag<Manager> managersBag;
-	
-	private Map<Class<?>, EntitySystem> systems;
-	private Bag<EntitySystem> systemsBag;
+						field.setAccessible(true);
+						field.set(target, world.getMapper(componentType));
+					}
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(
+						"Error while setting component mappers", e);
+			}
+		}
 
-	private static final Performer addedPerformer = new Performer() {
-			@Override
-			public void perform(EntityObserver observer, Entity e) {
-				observer.added(e);
-			}
-		};
-	private static final Performer deletedPerformer = new Performer() {
-			@Override
-			public void perform(EntityObserver observer, Entity e) {
-				observer.deleted(e);
-			}
-		};
-	private static final Performer changedPerformer = new Performer() {
-			@Override
-			public void perform(EntityObserver observer, Entity e) {
-				observer.changed(e);
-			}
-		};
-	private static final Performer enabledPerformer = new Performer() {
-			@Override
-			public void perform(EntityObserver observer, Entity e) {
-				observer.enabled(e);
-			}
-		};
-	private static final Performer disabledPerformer = new Performer() {
-			@Override
-			public void perform(EntityObserver observer, Entity e) {
-				observer.disabled(e);
-			}
-		};
+	}
+
+	/*
+	 * Only used internally to maintain clean code.
+	 */
+	private interface Performer {
+		void perform(EntityObserver observer, Entity e);
+	}
+
+	private final EntityManager								em;
+	private final ComponentManager							cm;
+	public float											delta;
+	private final Bag<Entity>								added;
+	private final Bag<Entity>								changed;
+	private final Bag<Entity>								deleted;
+
+	private final Bag<Entity>								enable;
+	private final Bag<Entity>								disable;
+
+	private final Map<Class<? extends Manager>, Manager>	managers;
+	private final Bag<Manager>								managersBag;
+
+	private final Map<Class<?>, EntitySystem>				systems;
+	private final Bag<EntitySystem>							systemsBag;
+	private static final Performer							addedPerformer		= new Performer() {
+																					@Override
+																					public void perform(
+																							EntityObserver observer,
+																							Entity e) {
+																						observer.added(e);
+																					}
+																				};
+	private static final Performer							deletedPerformer	= new Performer() {
+																					@Override
+																					public void perform(
+																							EntityObserver observer,
+																							Entity e) {
+																						observer.deleted(e);
+																					}
+																				};
+	private static final Performer							changedPerformer	= new Performer() {
+																					@Override
+																					public void perform(
+																							EntityObserver observer,
+																							Entity e) {
+																						observer.changed(e);
+																					}
+																				};
+	private static final Performer							enabledPerformer	= new Performer() {
+																					@Override
+																					public void perform(
+																							EntityObserver observer,
+																							Entity e) {
+																						observer.enabled(e);
+																					}
+																				};
+
+	private static final Performer							disabledPerformer	= new Performer() {
+																					@Override
+																					public void perform(
+																							EntityObserver observer,
+																							Entity e) {
+																						observer.disabled(e);
+																					}
+																				};
+
 	public World() {
 		managers = new HashMap<Class<? extends Manager>, Manager>();
 		managersBag = new Bag<Manager>();
-		
+
 		systems = new HashMap<Class<?>, EntitySystem>();
 		systemsBag = new Bag<EntitySystem>();
 
@@ -81,37 +126,93 @@ public class World {
 
 		cm = new ComponentManager();
 		setManager(cm);
-		
+
 		em = new EntityManager();
 		setManager(em);
 	}
 
-	
 	/**
-	 * Makes sure all managers systems are initialized in the order they were added.
-	 */
-	public void initialize() {
-		for (int i = 0; i < managersBag.size(); i++) {
-			managersBag.get(i).initialize();
-		}
-		
-		for (int i = 0; i < systemsBag.size(); i++) {
-			ComponentMapperInitHelper.config(systemsBag.get(i), this);
-			systemsBag.get(i).initialize();
-		}
-	}
-	
-	
-	/**
-	 * Returns a manager that takes care of all the entities in the world.
-	 * entities of this world.
+	 * Adds a entity to this world.
 	 * 
-	 * @return entity manager.
+	 * @param e
+	 *            entity
 	 */
-	public EntityManager getEntityManager() {
-		return em;
+	public void addEntity(Entity e) {
+		added.add(e);
 	}
-	
+
+	/**
+	 * Ensure all systems are notified of changes to this entity. If you're
+	 * adding a component to an entity after it's been added to the world, then
+	 * you need to invoke this method.
+	 * 
+	 * @param e
+	 *            entity
+	 */
+	public void changedEntity(Entity e) {
+		changed.add(e);
+	}
+
+	/**
+	 * Create and return a new or reused entity instance. Will NOT add the
+	 * entity to the world, use World.addEntity(Entity) for that.
+	 * 
+	 * @return entity
+	 */
+	public Entity createEntity() {
+		return em.createEntityInstance();
+	}
+
+	/**
+	 * Delete the entity from the world.
+	 * 
+	 * @param e
+	 *            entity
+	 */
+	public void deleteEntity(Entity e) {
+		if (!deleted.contains(e)) {
+			deleted.add(e);
+		}
+	}
+
+	/**
+	 * Deletes the manager from this world.
+	 * 
+	 * @param manager
+	 *            to delete.
+	 */
+	public void deleteManager(Manager manager) {
+		managers.remove(manager);
+		managersBag.remove(manager);
+	}
+
+	/**
+	 * Removed the specified system from the world.
+	 * 
+	 * @param system
+	 *            to be deleted from world.
+	 */
+	public void deleteSystem(EntitySystem system) {
+		systems.remove(system.getClass());
+		systemsBag.remove(system);
+	}
+
+	/**
+	 * Disable the entity from being processed. Won't delete it, it will
+	 * continue to exist but won't get processed.
+	 */
+	public void disable(Entity e) {
+		disable.add(e);
+	}
+
+	/**
+	 * (Re)enable the entity in the world, after it having being disabled. Won't
+	 * do anything unless it was already disabled.
+	 */
+	public void enable(Entity e) {
+		enable.add(e);
+	}
+
 	/**
 	 * Returns a manager that takes care of all the components in the world.
 	 * 
@@ -120,21 +221,34 @@ public class World {
 	public ComponentManager getComponentManager() {
 		return cm;
 	}
-	
-	
-	
 
 	/**
-	 * Add a manager into this world. It can be retrieved later.
-	 * World will notify this manager of changes to entity.
+	 * Time since last game loop.
 	 * 
-	 * @param manager to be added
+	 * @return delta time since last game loop.
 	 */
-	public <T extends Manager> T setManager(T manager) {
-		managers.put(manager.getClass(), manager);
-		managersBag.add(manager);
-		manager.setWorld(this);
-		return manager;
+	public float getDelta() {
+		return delta;
+	}
+
+	/**
+	 * Get a entity having the specified id.
+	 * 
+	 * @param entityId
+	 * @return entity
+	 */
+	public Entity getEntity(int entityId) {
+		return em.getEntity(entityId);
+	}
+
+	/**
+	 * Returns a manager that takes care of all the entities in the world.
+	 * entities of this world.
+	 * 
+	 * @return entity manager.
+	 */
+	public EntityManager getEntityManager() {
+		return em;
 	}
 
 	/**
@@ -148,109 +262,29 @@ public class World {
 	public <T extends Manager> T getManager(Class<T> managerType) {
 		return managerType.cast(managers.get(managerType));
 	}
-	
-	/**
-	 * Deletes the manager from this world.
-	 * @param manager to delete.
-	 */
-	public void deleteManager(Manager manager) {
-		managers.remove(manager);
-		managersBag.remove(manager);
-	}
 
-	
-	
-	
 	/**
-	 * Time since last game loop.
+	 * Retrieves a ComponentMapper instance for fast retrieval of components
+	 * from entities.
 	 * 
-	 * @return delta time since last game loop.
+	 * @param type
+	 *            of component to get mapper for.
+	 * @return mapper for specified component type.
 	 */
-	public float getDelta() {
-		return delta;
+	public <T extends Component> ComponentMapper<T> getMapper(Class<T> type) {
+		return ComponentMapper.getFor(type, this);
 	}
 
 	/**
-	 * You must specify the delta for the game here.
+	 * Retrieve a system for specified system type.
 	 * 
-	 * @param delta time since last game loop.
+	 * @param type
+	 *            type of system.
+	 * @return instance of the system in this world.
 	 */
-	public void setDelta(float delta) {
-		this.delta = delta;
+	public <T extends EntitySystem> T getSystem(Class<T> type) {
+		return type.cast(systems.get(type));
 	}
-	
-
-
-	/**
-	 * Adds a entity to this world.
-	 * 
-	 * @param e entity
-	 */
-	public void addEntity(Entity e) {
-		added.add(e);
-	}
-	
-	/**
-	 * Ensure all systems are notified of changes to this entity.
-	 * If you're adding a component to an entity after it's been
-	 * added to the world, then you need to invoke this method.
-	 * 
-	 * @param e entity
-	 */
-	public void changedEntity(Entity e) {
-		changed.add(e);
-	}
-	
-	/**
-	 * Delete the entity from the world.
-	 * 
-	 * @param e entity
-	 */
-	public void deleteEntity(Entity e) {
-		if (!deleted.contains(e)) {
-			deleted.add(e);
-		}
-	}
-
-	/**
-	 * (Re)enable the entity in the world, after it having being disabled.
-	 * Won't do anything unless it was already disabled.
-	 */
-	public void enable(Entity e) {
-		enable.add(e);
-	}
-
-	/**
-	 * Disable the entity from being processed. Won't delete it, it will
-	 * continue to exist but won't get processed.
-	 */
-	public void disable(Entity e) {
-		disable.add(e);
-	}
-
-
-	/**
-	 * Create and return a new or reused entity instance.
-	 * Will NOT add the entity to the world, use World.addEntity(Entity) for that.
-	 * 
-	 * @return entity
-	 */
-	public Entity createEntity() {
-		return em.createEntityInstance();
-	}
-
-	/**
-	 * Get a entity having the specified id.
-	 * 
-	 * @param entityId
-	 * @return entity
-	 */
-	public Entity getEntity(int entityId) {
-		return em.getEntity(entityId);
-	}
-
-	
-
 
 	/**
 	 * Gives you all the systems in this world for possible iteration.
@@ -262,9 +296,73 @@ public class World {
 	}
 
 	/**
+	 * Makes sure all managers systems are initialized in the order they were
+	 * added.
+	 */
+	public void initialize() {
+		for (int i = 0; i < managersBag.size(); i++) {
+			managersBag.get(i).initialize();
+		}
+
+		for (int i = 0; i < systemsBag.size(); i++) {
+			ComponentMapperInitHelper.config(systemsBag.get(i), this);
+			systemsBag.get(i).initialize();
+		}
+	}
+
+	/**
+	 * Process all non-passive systems.
+	 */
+	public void process() {
+		check(added, addedPerformer);
+
+		check(changed, changedPerformer);
+
+		check(disable, disabledPerformer);
+
+		check(enable, enabledPerformer);
+
+		check(deleted, deletedPerformer);
+
+		cm.clean();
+
+		for (int i = 0; systemsBag.size() > i; i++) {
+			EntitySystem system = systemsBag.get(i);
+			if (!system.isPassive()) {
+				system.process();
+			}
+		}
+	}
+
+	/**
+	 * You must specify the delta for the game here.
+	 * 
+	 * @param delta
+	 *            time since last game loop.
+	 */
+	public void setDelta(float delta) {
+		this.delta = delta;
+	}
+
+	/**
+	 * Add a manager into this world. It can be retrieved later. World will
+	 * notify this manager of changes to entity.
+	 * 
+	 * @param manager
+	 *            to be added
+	 */
+	public <T extends Manager> T setManager(T manager) {
+		managers.put(manager.getClass(), manager);
+		managersBag.add(manager);
+		manager.setWorld(this);
+		return manager;
+	}
+
+	/**
 	 * Adds a system to this world that will be processed by World.process()
 	 * 
-	 * @param system the system to add.
+	 * @param system
+	 *            the system to add.
 	 * @return the added system.
 	 */
 	public <T extends EntitySystem> T setSystem(T system) {
@@ -273,55 +371,26 @@ public class World {
 
 	/**
 	 * Will add a system to this world.
-	 *  
-	 * @param system the system to add.
-	 * @param passive wether or not this system will be processed by World.process()
+	 * 
+	 * @param system
+	 *            the system to add.
+	 * @param passive
+	 *            wether or not this system will be processed by World.process()
 	 * @return the added system.
 	 */
 	public <T extends EntitySystem> T setSystem(T system, boolean passive) {
 		system.setWorld(this);
 		system.setPassive(passive);
-		
+
 		systems.put(system.getClass(), system);
 		systemsBag.add(system);
-		
+
 		return system;
 	}
-	
-	/**
-	 * Removed the specified system from the world.
-	 * @param system to be deleted from world.
-	 */
-	public void deleteSystem(EntitySystem system) {
-		systems.remove(system.getClass());
-		systemsBag.remove(system);
-	}
-	
-	private void notifySystems(Performer performer, Entity e) {
-		for(int i = 0, s=systemsBag.size(); s > i; i++) {
-			performer.perform(systemsBag.get(i), e);
-		}
-	}
 
-	private void notifyManagers(Performer performer, Entity e) {
-		for(int a = 0; managersBag.size() > a; a++) {
-			performer.perform(managersBag.get(a), e);
-		}
-	}
-	
-	/**
-	 * Retrieve a system for specified system type.
-	 * 
-	 * @param type type of system.
-	 * @return instance of the system in this world.
-	 */
-	public <T extends EntitySystem> T getSystem(Class<T> type) {
-		return type.cast(systems.get(type));
-	}
-
-	
 	/**
 	 * Performs an action on each entity.
+	 * 
 	 * @param entities
 	 * @param performer
 	 */
@@ -336,72 +405,16 @@ public class World {
 		}
 	}
 
-	
-	/**
-	 * Process all non-passive systems.
-	 */
-	public void process() {
-		check(added, addedPerformer);
-		
-		check(changed, changedPerformer);
-		
-		check(disable, disabledPerformer);
-		
-		check(enable, enabledPerformer);
-		
-		check(deleted, deletedPerformer);
-		
-		cm.clean();
-		
-		for(int i = 0; systemsBag.size() > i; i++) {
-			EntitySystem system = systemsBag.get(i);
-			if(!system.isPassive()) {
-				system.process();
-			}
+	private void notifyManagers(Performer performer, Entity e) {
+		for (int a = 0; managersBag.size() > a; a++) {
+			performer.perform(managersBag.get(a), e);
 		}
 	}
-	
 
-	/**
-	 * Retrieves a ComponentMapper instance for fast retrieval of components from entities.
-	 * 
-	 * @param type of component to get mapper for.
-	 * @return mapper for specified component type.
-	 */
-	public <T extends Component> ComponentMapper<T> getMapper(Class<T> type) {
-		return ComponentMapper.getFor(type, this);
-	}
-	
-
-	/*
-	 * Only used internally to maintain clean code.
-	 */
-	private interface Performer {
-		void perform(EntityObserver observer, Entity e);
-	}
-
-	
-	
-	private static class ComponentMapperInitHelper {
-
-		public static void config(Object target, World world) {
-			try {
-				Class<?> clazz = target.getClass();
-				for (Field field : clazz.getDeclaredFields()) {
-					Mapper annotation = field.getAnnotation(Mapper.class);
-					if (annotation != null && Mapper.class.isAssignableFrom(Mapper.class)) {
-						ParameterizedType genericType = (ParameterizedType) field.getGenericType();
-						Class componentType = (Class) genericType.getActualTypeArguments()[0];
-
-						field.setAccessible(true);
-						field.set(target, world.getMapper(componentType));
-					}
-				}
-			} catch (Exception e) {
-				throw new RuntimeException("Error while setting component mappers", e);
-			}
+	private void notifySystems(Performer performer, Entity e) {
+		for (int i = 0, s = systemsBag.size(); s > i; i++) {
+			performer.perform(systemsBag.get(i), e);
 		}
-
 	}
 
 }
